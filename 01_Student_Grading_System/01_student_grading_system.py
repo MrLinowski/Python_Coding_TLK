4712bcfd939333545c04e3eb5027f02e01bf90ed
MrLinowski <p0170318@pdsb.net>
2025-02-24 11:43:04 -0500

    Initial Commit

---
+++ 01_Student_Grading_System/01_student_grading_system.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------
+# Name:        Student grading system (.py)
+# Purpose:     To provide a sample program about the basics of Python
+#
+# Author:      Mr. Seidel
+# Created:     14-Aug-2018
+# Updated:     28-Sep-2018
+#-----------------------------------------------------------------------------
+print("Please enter your score out of a 100")
+score = int(input)
