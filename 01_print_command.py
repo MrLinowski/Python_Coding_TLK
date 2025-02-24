428893f9a749a2305daad52ec45c9412e9a2839e
MrLinowski <p0170318@pdsb.net>
2025-02-24 07:30:18 -0500

    Initial Commit

---
+++ 01_print_command.py
@@ -0,0 +1,22 @@
+# This is my first code ever
+print("Hello Class")
+myName = input("What is your name? :")
+# this is a
+
+#I added an age variable Feb 19 ,2025
+myAge = input("What is your age? :")
+# I added the colour variable
+myColour = input("What is your favorite colour? :")
+print(myName)
+
+# Adding output for the remaining variables
+print("Your age is:", myAge)
+print("Your favorite color is:", myColour)
+
+
+
+#This is where I am trying it again on my own
+#print("kjashdkasjh asd ")
+#print("kjashdkasjh asd ")
+#print("kjashdkasjh asd ")
+#print("kjashdkasjh asd ")
