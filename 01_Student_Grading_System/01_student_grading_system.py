71777825374855d88fa54f1d301ca7e106950efc
MrLinowski <p0170318@pdsb.net>
2025-02-28 08:17:55 -0500

    updated student grading system

--- 01_Student_Grading_System/01_student_grading_system.py
+++ 01_Student_Grading_System/01_student_grading_system.py
@@ -2,9 +2,9 @@
 # Name:        Student grading system (.py)
 # Purpose:     To provide a sample program about the basics of Python
 #
-# Author:      Mr. Seidel
+# Author:      Mr. Linowski
 # Created:     14-Aug-2018
 # Updated:     28-Sep-2018
 #-----------------------------------------------------------------------------
-print("Please enter your score out of a 100")
-score = int(input)
+myScore = int(input("Please enter your score out of a 100"))
+#score = int(input)
